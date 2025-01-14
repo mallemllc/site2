80f96075a2958c04286fcfa8e5d5e78c38d272e4
ujjwal mallem <ujjwal.mallem@gmail.com>
2025-01-14 14:25:22 -0500

    bugfixes - footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -56,7 +56,7 @@ const Footer = ({ t }: { t: TFunction }) => {
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
+              <a href="mailto:support@mallemreseau.com">
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
@@ -126,6 +126,7 @@ const Footer = ({ t }: { t: TFunction }) => {
                   height="64px"
                 />
               </LogoContainer>
+            <FooterContainer>
             </NavLink>
               <SocialLink
                 href="https://www.linkedin.com/in/ujjwalmallem/"
